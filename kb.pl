a41962ae133815185e3f8d0eafc8a65a28c097a9
bhavya <bhavya25hirani@gmail.com>
2024-02-10 22:11:11 +0530

    working solution
    used variables set through nb_setval and b_setval to keep track of state, passed them through function arguments, performed appropriate state checks such as if current path cost was lesser than shortestPathCost so far. If yes, shortestPathCost was updated. shortest path like this was updated multiple times, and due to a lack of global variables, I was not able to find a way to print just the last occurrence --- i.e. just the final shortest path. Temporarily, I used asserta/1 to add successive occurrences as predicates one above the other, and simply called that predicate in the terminal to get the answer.

--- kb.pl
+++ kb.pl
@@ -1,3 +1,4 @@
+% declaring atoms as dynamic allows us to make changes to them at runtime through statements like assert and retract
 :- dynamic(path/3).
 :- dynamic(visited/1).
 :- dynamic(shortestPath/1).
@@ -37,24 +38,7 @@ path(e, d, 11).
 path(e, f, 6).
 path(e, g, 7).
 
-% ------- OVERLOAD FOR THE TRAVEL FUNCTION THAT THE USER CALLS -------
-% This overload will create the necessary non-backtrackable variables required to maintain the following state information:
-% 1. number of nodes in the graph --- numOfNodes
-% 2. the shortest path so far --- shortestPath
-% 3. the shortest path length so far --- shortestPathLength
-
-travel(Start, End, PathSoFar) :-
-	nb_setval(numOfNodes, 7),
-	nb_setval(shortestPathLength, 1000), % setting some arbitrarily high initial value for shortestPathLength
-	nb_setval(shortestPath, [j, k, l, m, n, o, p, q]), % empty list as initial value for shortests path
-
-	travel(Start, End, PathSoFar, numOfNodes),
-
-	write(' shortest path: '),
-	nb_getval(shortestPath, ShortestPath),
-	print_list(ShortestPath).
-
-% ------- BASE AND RECURSIVE CASES FOR PRINTING ELEMENTS IN A LIST -------
+% ------- BASE AND RECURSIVE CASES FOR PRINTING ELEMENTS IN A LIST (Was required mainly for debugging) -------
 print_list(List) :-
 	length(List, X),
    	X =:= 1,
@@ -72,30 +56,67 @@ print_list(List) :-
 	format('-- ~w ', [H]),
 	print_list(T).
 
+% ------- OVERLOAD FOR THE TRAVEL FUNCTION THAT THE USER CALLS -------
+% This overload will create the necessary non-backtrackable variables required to maintain the following state information:
+% 1. number of nodes in the graph --- numOfNodes
+% 2. the shortest path so far --- shortestPath --- stored in a non-backtrackable variable
+% 3. the shortest path length so far --- shortestPathLength --- stored in a non-backtrackable variable
+% 4. Cost of the Current path we're on --- CurrentPathCost --- stored as a backtrackable variable, because we want changes (addition of iterative costs) to be reversed when we backtrack and take another path
+travel(Start, End, PathSoFar) :-
+	nb_setval(numOfNodes, 7),
+	nb_setval(shortestPathCost, 1000), % setting some arbitrarily high initial value for shortestPathLength
+	nb_setval(shortestPath, [j, k, l, m, n, o, p, q]), % empty list as initial value for shortests path
+	b_setval(currentPathCost, 0),
+
+	travel(Start, End, PathSoFar, numOfNodes, shortestPathCost, shortestPath, currentPathCost),
+
+	%write(' shortest path: '),
+	nb_getval(shortestPath, ShortestPath),
+	asserta(shortestPath(ShortestPath)).
+	%print_list(ShortestPath).
+
+
 % ------- BASE AND RECURSIVE CASES FOR TRAVELLING BETWEEN TWO NODES -------
-travel(Start, End, PathSoFar, numOfNodes) :-
-	%format('base case travel with Start = ~w, End = ~w, and PathSoFar = ~w', [Start, End, PathSoFar]),
-	%nl,
-	% If there exists a direct path between this node (Start) and the finish, take that path. Now PathSoFar is officially
-	% the path from Start to End. Hence we can print it.
-	path(Start, End, _ ), 
+travel(Start, End, PathSoFar, numOfNodes, shortestPathCost, shortestPath, currentPathCost) :-
+	% Base case for travel: Start is the second-last node and End is the destination
+	% First append End to the PathSoFar variable
+	path(Start, End, Cost), 
 	not(member(End, PathSoFar)),
 	append(PathSoFar, [End], PathSoFar1),
 
+	% Then check if the path is passing through all nodes (check its length)
 	length(PathSoFar1, PathSoFar1Length),
 	nb_getval(numOfNodes, NumberOfNodes),
-	PathSoFar1Length =:= NumberOfNodes.
+	PathSoFar1Length =:= NumberOfNodes,
+	
+	%write('hi1'),
+	% Consider this solution only if it has total costs lesser than shortestPathCost
+	b_getval(currentPathCost, CurrentPathCost),
+	nb_getval(shortestPathCost, ShortestPathCost),
+	UpdatedPathCost is CurrentPathCost + Cost,
+	b_setval(currentPathCost, UpdatedPathCost),
+	
+	% if so, update ShortestPathCost and ShortestPath values
+	UpdatedPathCost @< ShortestPathCost, 
+	%ShortestPathCost is UpdatedPathCost,
+	nb_setval(shortestPathCost, UpdatedPathCost),
+	nb_setval(shortestPath, PathSoFar1).
 
-	%print_list(PathSoFar1).
 
-travel(Start, End, PathSoFar, numOfNodes) :-
+travel(Start, End, PathSoFar, numOfNodes, shortestPathCost, shortestPath, currentPathCost) :-
 	%format('recursive case travel with Start = ~w, End = ~w, and PathSoFar = ~w', [Start, End, PathSoFar]),
 	%nl,
 	% If no direct path exist between Start and End, Consider paths between Start and it's neighbours, 
 	% Add this neighbor to PathSoFar if not already there (avoiding loops), 
+	% update the current path cost
 	% and then recursively calculate the paths between that neighbour and End.
-	path(Start, Neigh, _), 
+	path(Start, Neigh, Cost), 
 	not(member(Neigh, PathSoFar)),
 	append(PathSoFar, [Neigh], PathSoFar1),	
-	travel(Neigh, End, PathSoFar1, numOfNodes).
+
+	b_getval(currentPathCost, CurrentPathCost),
+	UpdatedPathCost is CurrentPathCost + Cost,
+	b_setval(currentPathCost, UpdatedPathCost),
+
+	travel(Neigh, End, PathSoFar1, numOfNodes, shortestPathCost, shortestPath, currentPathCost).
 
